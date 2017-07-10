91804f4f8442e08a8f3173a32d37c28bc9563a23
ncarvalho <nunocarvalhodossatos@gmail.com>
2017-07-10 23:16:52 +0200

    Update siane_plot

--- R/siane_plot.R
+++ R/siane_plot.R
@@ -18,12 +18,12 @@
 
 
 
-#' @details - \code{shp} is a S4 object. \cr
-#' \code{pallete_colour} is a parameter from the /code{RColorBrewer::brewer.pal} function. The default pallete is "OrRd". Check RColorBrewer:display.brewer.all() to list all possible colours.
-#' \code{n} is a parameter from the /code{classInt::classIntervals} function. Default  \code{n = 5}
-#' \code{style} is a parameter from the /code{classInt::classIntervals} function. Defauly \code{style = quantile} Check ?classInt::classIntervals to list more options.
-#' \code{...} other parameters can be set to the legend() function and the title() function.
-#'Please check ?legend and ?title to learn about the parameters of these functions.
+#' @details - \code{shp} is a S4 object returned by the \code{siane_merge} function . \cr
+#' \code{pallete_colour} is a parameter from the \code{RColorBrewer::brewer.pal} function. The default pallete is "OrRd". Check RColorBrewer:display.brewer.all() to list all possible colours.
+#' \code{n} is a parameter from the /code{classInt::classIntervals} function. Default  \code{n = 5}  \cr
+#' \code{style} is a parameter from the /code{classInt::classIntervals} function. Defauly \code{style = quantile} Check ?classInt::classIntervals to list more options. \cr
+#' \code{...} other parameters can be set to the legend() function and the title() function. 
+#'Please check ?legend and ?title to learn about the parameters of these functions. \cr
 
 #' @export
 
